02c923c32cf5576a3cf371cd48b3c247b4205644
CodeJCSON <2334497007@qq.com>
2017-04-19 09:54:13 +0800

    指示器显示

--- LBXiongMaoTV/LBXiongMaoTV/home(主页)/LBXMHomeTitleView/LBXMHomeTitleView.swift
+++ LBXiongMaoTV/LBXiongMaoTV/home(主页)/LBXMHomeTitleView/LBXMHomeTitleView.swift
@@ -19,9 +19,8 @@ class LBXMHomeTitleView: UIView {
     var titleButtonArray:[UIButton] = [UIButton]()
     ///指示器视图
     let indicatorV:UIView = {
-        //cz_color(withRed: 111, green: 197, blue: 153)
         let indicatorView = UIView()
-        indicatorView.backgroundColor = UIColor.red
+        indicatorView.backgroundColor = UIColor.cz_color(withRed: 111, green: 197, blue: 153)
         return indicatorView
     }()
     ///标题滑动视图
@@ -36,16 +35,15 @@ class LBXMHomeTitleView: UIView {
         return titleScrollView
     }()
     
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-
+        ///指示器视图
+        indicatorV.height = 4
         addSubview(titleScrollView)
         titleScrollView.frame = self.bounds
         ///添加标题栏视图
         addHomeTitleScrollView()
-        ///指示器视图 cz_color(withRed: 111, green: 197, blue: 153)
-        indicatorV.height = 4
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -55,14 +53,15 @@ class LBXMHomeTitleView: UIView {
     func titleBtnClick(sender:UIButton){
  
         for btn in titleButtonArray {
-            btn.isSelected = false
-            btn.isUserInteractionEnabled = true
+            //btn.isSelected = false
+            //btn.isUserInteractionEnabled = true
+            btn.isEnabled = true
         }
         
-        titleButtonArray[sender.tag].isSelected = true
-        titleButtonArray[sender.tag].isUserInteractionEnabled = false
-        
-        UIView.animate(withDuration: 0.25) { 
+        //titleButtonArray[sender.tag].isSelected = true
+        //titleButtonArray[sender.tag].isUserInteractionEnabled = false
+        titleButtonArray[sender.tag].isEnabled = false
+        UIView.animate(withDuration: 0.25) {
           self.indicatorV.centerX = self.titleButtonArray[sender.tag].centerX
         }
     }
@@ -78,10 +77,9 @@ extension LBXMHomeTitleView{
             titleBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
             titleBtn.frame = CGRect.init(x: titleWidth * CGFloat(index), y: 0, width: titleWidth, height: titleHeight)
             titleBtn.addTarget(self, action: #selector(LBXMHomeTitleView.titleBtnClick(sender:)), for: .touchUpInside)
-            titleBtn.backgroundColor = UIColor.cz_random()
             titleBtn.setTitle(titleSting, for: .normal)
             titleBtn.setTitleColor(UIColor.cz_color(withRed: 127, green: 127, blue: 127), for: .normal)
-            titleBtn.setTitleColor(UIColor.cz_color(withRed: 111, green: 197, blue: 153), for: .selected)
+            titleBtn.setTitleColor(UIColor.cz_color(withRed: 111, green: 197, blue: 153), for: .disabled)
                 
             titleBtn.tag = index
             indicatorV.frame.origin.y = 40 - 4
@@ -89,18 +87,19 @@ extension LBXMHomeTitleView{
             titleScrollView.addSubview(titleBtn)
             ///默认选中第一个按钮
             if index == 0 {
-                titleBtn.isUserInteractionEnabled = false
-                titleBtn.isSelected = true
-                
+                //titleBtn.isUserInteractionEnabled = false
+                //titleBtn.isSelected = true
+                titleBtn.isEnabled = false
+                titleBtn.titleLabel?.sizeToFit()
+                indicatorV.width = titleBtn.titleLabel?.width ?? 0
                 indicatorV.centerX = titleBtn.centerX
-                indicatorV.width = (titleBtn.titleLabel?.width)!
-                titleBtn.addSubview(indicatorV)
+                //titleBtn.addSubview(indicatorV)
                 
-                print(indicatorV.centerX)
+                print(indicatorV.frame)
             }
             
         }
-        
+        titleScrollView.addSubview(indicatorV)
     }
 
 }
